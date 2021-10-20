0470f259cb63050dda9960105e54a658b51b5fed
jszabala <jszabala95@misena.edu.co>
2021-10-20 14:10:09 -0500

    primer commit, gateway de apis java

---
+++ src/test/java/com/senaSoft/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
